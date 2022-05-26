d66766aafe68380fda69a9b4b448e8f81b8ec327
Gavin <hereisgeek@gmail.com>
2022-05-26 16:28:20 +0800

    13

--- src/views/ThinkView.vue
+++ src/views/ThinkView.vue
@@ -6,43 +6,86 @@
       justify-center justify-items-center
       items-center
       flex flex-col
+      h-screen
     "
   >
-    <div class="w-screen" id="shot">
-      <div id="preview" class="flex justify-center items-center">
-        <div class="think-card w-3/5 shadow-xl" id="thinkCard">
-          <div class="avatar">
-            <div
-              class="
-                w-24
-                rounded-full
-                ring ring-primary ring-offset-base-100 ring-offset-2
-              "
-            >
-              <img
-                id="imgUpload"
-                src="https://cdn.lorem.space/images/face/.cache/500x0/sam-burriss-jTSf1xnsoCs-unsplash.jpg"
-              />
+    <div class="w-screen bg-slate-200" id="preview">
+      <div class="flex justify-center items-center p-1">
+        <div
+          class="
+            think-card
+            w-4/5
+            shadow-xl
+            flex flex-col
+            justify-start justify-items-start
+            items-start
+          "
+          id="thinkCard"
+        >
+          <div
+            class="
+              avatar
+              flex flex-row
+              justify-center justify-items-center
+              items-center
+            "
+          >
+            <div class="w-24 rounded-full">
+              <img id="imgUpload" src="@/assets/avatar.jpg" />
             </div>
+
+            <p style="font-size: 2rem; margin-left: 1rem">{{ userName }}</p>
+            <p style="font-size: 1rem; margin-left: 1rem; color: gray">
+              {{ currentTime }}
+            </p>
           </div>
-          <p style="word-break: break-all">{{ inputtext }}</p>
+          <p
+            style="
+              word-break: break-all;
+              font-size: larger;
+              margin-top: 1rem;
+              text-align: start;
+            "
+          >
+            {{ inputtext }}
+          </p>
         </div>
       </div>
-      <p>Your text</p>
-      <input type="text" class="input w-full max-w-xs" v-model="inputtext" />
-      <p>Border redius</p>
-      <input
-        type="range"
-        min="0"
-        max="30"
-        value="0"
-        class="range w-9/12"
-        id="rediusSelect"
-      />
     </div>
-    <input type="file" id="file" />
-    <button class="btn btn-primary" @click="uploadTheImg">Upload</button>
-    <button class="btn btn-primary" @click="downloadTheMock">Save</button>
+    <p class="mt-3">Your name</p>
+    <input type="text" class="input w-full max-w-xs" v-model="userName" />
+    <p>Your text</p>
+    <input type="text" class="input w-full max-w-xs" v-model="inputtext" />
+
+    <p>Border redius</p>
+    <input
+      type="range"
+      min="0"
+      max="30"
+      value="0"
+      class="range w-9/12"
+      id="rediusSelect"
+    />
+    <p>Card width size</p>
+    <input
+      type="range"
+      min="20"
+      max="60"
+      value="50"
+      class="range w-9/12"
+      id="sizeSelect"
+    />
+    <input type="file" id="file" class="mt-2" />
+    <div class="flex flex-row w-4/5">
+      <div class="basis-1/2">
+        <button class="btn btn-primary" @click="uploadTheImg">Upload</button>
+      </div>
+      <div class="basis-1/2">
+        <button class="btn btn-primary basis-1/2" @click="downloadTheMock">
+          Save
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -53,12 +96,15 @@ export default {
   data() {
     return {
       inputtext: "",
+      userName: "User",
+      currentTime: "",
     };
   },
   methods: {
-    //点击Save通过html2canvas将preview的子元素转换成图片，并下载
+    //点击Save通过html2canvas将preview的子元素转换成图片，并下载,并更新currentTime
     downloadTheMock() {
       html2canvas(document.getElementById("preview")).then((canvas) => {
+        this.currentTime = new Date().toLocaleString();
         let a = document.createElement("a");
         a.href = canvas.toDataURL("image/png");
         a.download = "mock.png";
@@ -75,12 +121,19 @@ export default {
       };
     },
   },
+  //监听id为numberSelect的value,更新selectNumber
+  watch: {},
   mounted() {
     document.getElementById("rediusSelect").addEventListener("input", (e) => {
       //thinkCard的border-radius属性与rediusSelect的value绑定
       document.getElementById("thinkCard").style.borderRadius =
         e.target.value + "px";
     });
+    document.getElementById("sizeSelect").addEventListener("input", (e) => {
+      //thinkCard的width属性与sizeSelect的value绑定
+      document.getElementById("thinkCard").style.width = e.target.value + "rem";
+    });
+    this.currentTime = new Date().toLocaleString();
   },
 };
 </script>
@@ -90,15 +143,13 @@ export default {
   height: 64px;
 }
 #preview {
-  height: 30rem;
   width: 100%;
-  padding: 1rem;
 }
 .think-card {
   height: 30rem;
   padding: 1rem;
-  border: 2px solid #000;
+  border: 1px solid #ccc;
   background-color: rgb(255, 255, 255);
-  box-shadow: 0 20px 25px -5px rgb(35, 35, 35), 0 8px 10px -6px rgb(35, 35, 35);
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 }
 </style>
